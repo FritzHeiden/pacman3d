2b1e5cc7823e47dba2f3b598dcf1c43a2e39a342
Kwutzke <Kwkw@me.com>
2017-02-10 17:46:07 +0100

    deployment fixed

--- src/main/java/game/Main.java
+++ src/main/java/game/Main.java
@@ -4,13 +4,17 @@ import engine.GameEngine;
 import engine.Scene;
 import game.scenes.*;
 
+import java.io.File;
+
 public class Main {
  
     public static void main(String[] args) {
         try {
+            System.setProperty("org.lwjgl.librarypath", new File("target/natives").getAbsolutePath());
+            System.out.println();
             boolean vSync = true;
-//            Scene scene = new MenuScene();
-            Scene scene = new GameScene();
+            Scene scene = new MenuScene();
+//            Scene scene = new GameScene();
             GameEngine gameEng = new GameEngine("Pacman 3D", 1280, 720, vSync, scene);
             gameEng.start();
         } catch (Exception excp) {
